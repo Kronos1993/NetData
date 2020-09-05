6d19b08ced8c30b4cef03bf1fe762f48a8460fea
Marcos Octavio Guerra Liso <mguerral1993@gmail.com>
2020-09-04 22:23:43 -0400

    Añadido nuevo id de notificación

--- app/src/main/java/com/kronos/netdata/Activities/Notifications/NotificationsId.java
+++ app/src/main/java/com/kronos/netdata/Activities/Notifications/NotificationsId.java
@@ -2,6 +2,6 @@ package com.kronos.netdata.Activities.Notifications;
 
 public enum NotificationsId {
 
-    paquete_buy,saldo,internet, error, bono , summary
+    paquete_buy,saldo,internet, error, bono , transferencia_saldo, summary
 
 }
